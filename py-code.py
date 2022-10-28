b6e545f596c903ed8c9247e5520796f8eebd9700
IngridNagell <ingrid.nagell@gjensidige.no>
2022-10-28 08:54:22 +0200

    Update.

--- py-code.py
+++
@@ -1,3 +0,0 @@
-#Importerer csv-fil med chat som ble definert av regex som "Lukke sak" og vurdert om det er riktig klassifisering
-relative_path = "../../data/skade_txt/skadesak_matches.xlsx"
-df = pd.read_excel(relative_path, header=1)
